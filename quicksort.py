452f2ec2bc9cead9909815e24dfa868a5f5f3f66
IntelligentCourage22 <78225784+IntelligentCourage22@users.noreply.github.com>
2021-04-14 21:50:48 +0530

    quicksort.py
    Main file

--- quicksort.py
+++ quicksort.py
@@ -15,11 +15,7 @@ def quickSort(arr, low, high):
 	if len(arr) == 1: 
 		return arr 
 	if low < high: 
-
-		
 		pi = partition(arr, low, high) 
-
-		
 		quickSort(arr, low, pi-1) 
 		quickSort(arr, pi+1, high) 
 
